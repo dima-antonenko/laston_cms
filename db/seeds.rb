f0df1c7d92615101b0c016ba23d1ee336c1991e7
dima_antonenko <d.antonenko94@mail.ru>
2017-03-22 10:23:50 +0300

    add seeds to menu

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 ProductCategory.destroy_all
 Product.destroy_all
+Menu.destroy_all
+MenuItem.destroy_all
 
 10.times do |i|
   ProductCategory.create(name: "Категория #{i}", description: "this is description")
@@ -19,18 +21,17 @@ first_category_id = ProductCategory.first.id
 end
 
 
+Menu.create(name: "главное меню", descriptor: "main_menu")
+Menu.create(name: "Меню в футоре", descriptor: "footer_menu")
 
-=begin
-    
-Menu.destroy_all
-MenuItem.destroy_all
-
-Menu.create(name: "главное меню", descriptor: "main")
 MenuItem.create(title: "Главная", menu_id: Menu.first.id, url: '/', position: 1)
 MenuItem.create(title: "Контакты", menu_id: Menu.first.id, url: '/contacts', position: 2)
 MenuItem.create(title: "О Компании", menu_id: Menu.first.id, url: '/about', position: 3)
 MenuItem.create(title: "Тестовая страница", menu_id: Menu.first.id, url: '/', position: 4)
+MenuItem.create(title: "Тестовая страница2", menu_id: Menu.first.id, menu_item_id: MenuItem.last.id, url: '/', position: 1)
 
-
-
-=end
+MenuItem.create(title: "Главная", menu_id: Menu.last.id, url: '/', position: 1)
+MenuItem.create(title: "Контакты", menu_id: Menu.last.id, url: '/contacts', position: 2)
+MenuItem.create(title: "О Компании", menu_id: Menu.last.id, url: '/about', position: 3)
+MenuItem.create(title: "Тестовая страница", menu_id: Menu.last.id, url: '/', position: 4)
+MenuItem.create(title: "Тестовая страница2", menu_id: Menu.last.id, menu_item_id: MenuItem.last.id, url: '/', position: 1)
